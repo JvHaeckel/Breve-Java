4f817db3cf5bbebb6152178af6e891c2108d6730
JvHaeckel <joaohaeckel@gmail.com>
2024-08-05 09:27:30 -0300

    SEBRAE

--- src/modularizacao/conversão_temperaturas.java
+++ src/modularizacao/conversão_temperaturas.java
@@ -2,33 +2,112 @@ package modularizacao;
 
 import java.util.Scanner;
 
-/*/*   Lista Modularização passou dia 20/11 a ser entregue 23/11
-2ª) Implemente um programa para fazer conversão de temperaturas em graus Fahrenheit para graus Celsius e vice-versa. O programa deverá apresentar os seguintes  subprogramas: 
+/*
+2ª) Implemente um programa para fazer conversão de temperaturas em graus Fahrenheit para graus Celsius e vice-versa. 
+O programa deverá apresentar os seguintes  subprogramas: 
 
-(a) Função para converter temperaturas em graus Fahrenheit para graus Celsius. Esta  função receberá como parâmetro de entrada o valor da temperatura em graus Fahrenheit a ser convertido e deverá calcular e retornar para o programa o valor  correspondente em graus Celsius. O programa se encarrega de exibir o resultado  ao usuário.  
+(a) Função para converter temperaturas em graus Fahrenheit para graus Celsius. Esta  função receberá como parâmetro 
+de entrada o valor da temperatura em graus Fahrenheit a ser convertido e deverá calcular e retornar para o programa o 
+valor  correspondente em graus Celsius. O programa se encarrega de exibir o resultado  ao usuário.  
 
-(b) Função para converter temperaturas em graus Celsius para graus Fahrenheit. Esta  função receberá como parâmetro de entrada o valor da temperatura em graus Celsius  a ser convertido e deverá calcular e retornar para o programa o valor correspondente  em graus Fahrenheit.
+(b) Função para converter temperaturas em graus Celsius para graus Fahrenheit. Esta  função receberá como parâmetro de
+ entrada o valor da temperatura em graus Celsius  a ser convertido e deverá calcular e retornar para o programa o valor 
+ correspondente  em graus Fahrenheit.
 
  O programa se encarrega de exibir o resultado ao usuário. 
-OBS1: O programa deverá permitir que o usuário faça quantas conversões desejar e só  deverá terminar sua execução se o usuário solicitar.  
+OBS1: O programa deverá permitir que o usuário faça quantas conversões desejar e só  deverá terminar sua execução se o 
+usuário solicitar.  
 
 OBS2: Toda entrada e saída de dados deve ser feita pelo programa principal. 
 
 OBS3: Para as conversões, utilize as fórmulas abaixo: 
 
-=F 
-
-F = C *1.8 + 32 
-C
-( − 32) 1.8 
-
+F = C * 1.8 + 32 
+C = (fahr - 32) / 1.8;
 
  */
 
 public class conversão_temperaturas {
 
-	public static void main(String[] args) {
+	
+	public static void menu() {
+
+		System.out.println("Deseja converter de Celsius para Fah digite 1");
+		System.out.println("Deseja converter de Fah para Celsius digite 2");
+		System.out.println("Para finalizar digite 0");
+		System.out.print("Digite sua opção: ");
 
 	}
+	
+	public static double farToCelsius(double temp) {
+
+		double result = (temp - 32) / 1.8;
+		return result;
+	}
+
+	public static double celsiusToFar(double temp) {
+
+		double result = (temp * 1.8) + 32;
+		return result;
+	}
+
+
+	
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int option;
+		double celsius, fah;
+
+		do {
+			menu();
+			option = in.nextInt();
+
+			switch (option) {
+
+			case 1:
+				System.out.println("Digite a temperatura em Celsius: ");
+				celsius = in.nextDouble();
+				in.nextLine();
+				while (celsius < -273) {
+					System.out.println("Error digite novamente: ");
+					celsius = in.nextDouble();
+					in.nextLine();
+				}
+				
+				fah = celsiusToFar(celsius);
+				System.out.println("O valor foi de: " + fah);
+
+				
+				break;
+
+			case 2:
+				System.out.println("Digite a temperatura em Fah: ");
+				fah = in.nextDouble();
+				in.nextLine();
+				while (fah < -459) {
+					System.out.println("Error digite novamente: ");
+					fah = in.nextDouble();
+					in.nextLine();
+				}
+				celsius = farToCelsius(fah);
+				System.out.println("O valor foi de: " + celsius);
+				break;
+
+			case 0:
+				System.out.println("Fim");
+				break;
+
+			default:
+				System.out.println("Error ");
+
+			}
+
+		} while (option != 0);
+
+	}
+
+	
 
 }
